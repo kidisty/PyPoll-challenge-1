3b1cd885a766c8ad00d2a1e6607a8b5ce6439408
kidist  yohannes <kidistyohannes@kidists-MacBook-Pro.local>
2020-07-07 01:03:35 -0700

    updated files

--- PyPoll.py
+++ PyPoll.py
@@ -4,7 +4,7 @@
 import csv
 import os
 
-# Files to load and output (Remember to change these)
+# Files to load and output 
 file_to_load = os.path.join("Resources", "election_data.csv")
 file_to_output = os.path.join("analysis", "election_analysis.txt")
 
